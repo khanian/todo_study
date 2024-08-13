f5a17cd0af59f2dfb78bd2dc357d09d29e1a2b5f
khanian <khanylee@gmail.com>
2024-08-13 17:52:41 +0900

    add about me

--- lib/mainscreen.dart
+++ lib/mainscreen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_study/add_task.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -11,7 +14,28 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List<String> todoList = [];
+
   void addTodo({required String todoText}) {
+    if (todoList.contains(todoText)) {
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("Already exists"),
+            content: Text("This task is already exists"),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: Text("Close"),
+              ),
+            ],
+          );
+        },
+      );
+      return;
+    }
     setState(() {
       todoList.insert(0, todoText);
     });
@@ -42,8 +66,34 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        drawer: const Drawer(
-          child: Text("Drawer"),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: const Text("khany Li"),
+                accountEmail: const Text("khanian@naver.com"),
+                currentAccountPicture: CircleAvatar(
+                  child: ClipOval(
+                    child: Image.asset("assets/images/khanyli.png"),
+                  ),
+                ),
+              ),
+              ListTile(
+                onTap: (){
+                  launchUrl(Uri.parse("https://www.youtube.com/@khanyli"));
+                },
+                leading: const Icon(Icons.youtube_searched_for_rounded),
+                title: Text("About Me"),
+              ),
+              ListTile(
+                onTap: (){
+                  launchUrl(Uri.parse("https://www.facebook.com/khany.liegh"));
+                },
+                leading: const Icon(Icons.facebook_rounded),
+                title: Text("About Me"),
+              ),
+            ],
+          ),
         ),
         appBar: AppBar(
           title: const Text("TODO App"),
